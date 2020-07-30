0419d07ea322c1e29134b200cf98cfbac3052a85
Syzygianinfern0 <spsharan2000@gmail.com>
2020-07-30 23:00:26 +0530

    Downloader for images

---
+++ downloader.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+
+
+def download_url(url, folder="downloads"):
+    print("downloading: ", url)
+
+    file_name_start_pos = url.rfind("/") + 1
+    file_name = url[file_name_start_pos:]
+
+    try:
+        r = requests.get(url, stream=True)
+        if r.status_code == requests.codes.ok:
+            with open(os.path.join(folder, file_name), 'wb') as f:
+                for data in r:
+                    f.write(data)
+        return True
+    except ConnectionError:
+        print("Invalid URL")
+        return False
