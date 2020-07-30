2a56cdf79590991c0ef73211db2ba861ce119202
Syzygianinfern0 <spsharan2000@gmail.com>
2020-07-30 22:49:32 +0530

    Detection of image

--- havenscraper.py
+++ havenscraper.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from wallhaven import Wallhaven, Parameters
+import pandas as pd
 
 load_dotenv()
 
@@ -11,6 +12,15 @@ API_KEY = os.getenv("API_KEY")
 
 if __name__ == '__main__':
     params.filter_by_user("RaidyHD")
+    params.set_purity(True, True, True)
 
-    data = wallhaven.search(params)
-    print(data)
+    latest_scrape = wallhaven.search(params)
+
+    local_data = pd.read_csv("walls.csv")
+    # print(local_data)
+
+    for wall in latest_scrape:
+        if (local_data['id'] == wall['id']).any():
+            print("there")
+        else:
+            print("not there")
