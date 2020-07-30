3f1acc3cfab717b22542043a7e7f7eaeeaeeb17e
Syzygianinfern0 <spsharan2000@gmail.com>
2020-07-30 23:52:12 +0530

    Cleanup

--- havenscraper.py
+++ havenscraper.py
@@ -1,10 +1,11 @@
+import csv
 import os
 
+import numpy as np
 from dotenv import load_dotenv
 from wallhaven import Wallhaven, Parameters
+
 from downloader import download_url
-import csv
-import numpy as np
 
 load_dotenv()
 
@@ -24,21 +25,19 @@ if __name__ == "__main__":
             local_data = np.array(list(reader))
     else:
         local_data = np.array([['id', 'downloaded', 'url'], ])
-    # local_data = pd.read_csv("walls.csv")
-    # print(local_data)
+
     new_downloads = list()
 
     for wall in latest_scrape[:5]:
         if wall["id"] not in local_data[:, 0]:
             download_path = wall["path"]
-            # status = download_url(download_path)
+            status = download_url(download_path)
             status = True
             local_data = np.vstack((local_data, [wall["id"], status, download_path]))
             new_downloads.append([wall["id"], status, download_path])
 
     print(new_downloads)
 
-    # np.savetxt("walls.csv", local_data, delimiter=",")
     with open("walls.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(local_data.tolist())
